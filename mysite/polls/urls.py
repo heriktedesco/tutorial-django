3054c7404227542fc466f2982c890d3536ca9113
heriktedesco <herik.tedesco@al.infnet.edu.br>
2019-04-11 06:36:45 -0300

    create part 3

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,17 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+
+    #ex: /polls/5/ - example url to get a question_id
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    #ex: /polls/id/result - example url to get a result
+    path('<int:question_id>/results/', views.results, name="results"),
+
+    #ex: polls/5/vote - example url to get a vote
+    path('<int:question_id>/vote/', views.vote, name="vote"),
 ]
